7a6bc16cb673d354098319052de8370a8807a8b6
GiovanniTorrisi28 <TRRGNN02A28C351N@studium.unict.it>
2023-08-09 17:09:57 +0200

    Aggiunta tab in home

--- app/src/main/java/com/example/appcarsharing/HomeFragment.java
+++ app/src/main/java/com/example/appcarsharing/HomeFragment.java
@@ -4,25 +4,36 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeFragment extends Fragment {
 
     private Button logout;
-
+    private View rootView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+         rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        ViewPager viewPager = rootView.findViewById(R.id.viewPager);
+
+        TabPagerAdapter adapter = new TabPagerAdapter(getParentFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = rootView.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
 
         //pulsante di logout
-        logout = rootView.findViewById(R.id.logout);
+       /* logout = rootView.findViewById(R.id.logout);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {    //logut utente con ritorno alla schermata login
@@ -31,9 +42,22 @@ public class HomeFragment extends Fragment {
                 startActivity(intent);
             }
         });
-
+*/
 
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ViewPager viewPager = rootView.findViewById(R.id.viewPager);
+
+        TabPagerAdapter adapter = new TabPagerAdapter(getParentFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = rootView.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
 
---
+++ app/src/main/java/com/example/appcarsharing/TabPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.appcarsharing;
+
+import android.os.Parcelable;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TabPagerAdapter extends FragmentStatePagerAdapter {
+    private static final int NUM_TABS = 2;
+    private List<CharSequence> titles = Arrays.asList("Chiedi Passaggio","Offri Passaggio");
+    public TabPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return new NotificationFragment(); // Il tuo fragment per "Chiedi Passaggio"
+            case 1:
+                return new SettingsFragment(); // Il tuo fragment per "Offri Passaggio"
+            default:
+                return new SettingsFragment();
+        }
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return titles.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_TABS;
+    }
+}
