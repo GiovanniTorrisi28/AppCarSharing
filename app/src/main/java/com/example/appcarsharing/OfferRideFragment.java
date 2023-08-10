b245a24de0368df0858f2a4a58c960ceb231348e
GiovanniTorrisi28 <TRRGNN02A28C351N@studium.unict.it>
2023-08-10 12:42:52 +0200

    salvato lo stato di orario, data e veicolo in offri passaggio

--- app/src/main/java/com/example/appcarsharing/OfferRideFragment.java
+++ app/src/main/java/com/example/appcarsharing/OfferRideFragment.java
@@ -5,6 +5,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.os.Bundle;
 
@@ -19,13 +20,17 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.HashMap;
+
 public class OfferRideFragment extends Fragment {
 
+    private View rootView;
     private ImageView calendarIcon,timeIcon,carIcon;
     private DatePickerDialog.OnDateSetListener dateSetListener;
     private TimePickerDialog.OnTimeSetListener timeSetListener;
@@ -33,18 +38,40 @@ public class OfferRideFragment extends Fragment {
     private EditText sourceEditText;
     private EditText destinationEditText;
 
+    private boolean[] fieldFilled = new boolean[1];
+
+    private String textTarga,textDettagli;
+    HashMap<String, Integer> date,time;
+
+    public OfferRideFragment() {
+        date = new HashMap<>();
+        time = new HashMap<>();
+        date.put("day",0);
+        date.put("month",0);
+        date.put("year",0);
+        time.put("hour",-1);
+        time.put("minute",-1);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_offer_ride, container, false);
+        rootView = inflater.inflate(R.layout.fragment_offer_ride, container, false);
+
+        for(int i=0;i< fieldFilled.length;i++)
+                fieldFilled[i] = false;
+
+        setFieldColor();
+
 
         calendarIcon = rootView.findViewById(R.id.calendar_icon);
-
         // Imposta il listener per gestire la selezione della data
         dateSetListener = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
                 // Qui puoi gestire la data selezionata dall'utente
+                date.put("day",dayOfMonth);
+                date.put("month",month);
+                date.put("year",year);
             }
         };
 
@@ -52,15 +79,18 @@ public class OfferRideFragment extends Fragment {
         calendarIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDatePicker();
+                showDatePicker(date.get("day"),date.get("month"),date.get("year"));
             }
         });
 
+        //icona orologio
         timeIcon = rootView.findViewById(R.id.time_icon);
         timeSetListener = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
                 // Qui puoi gestire l'orario selezionato dall'utente
+                time.put("hour",hourOfDay);
+                time.put("minute",minute);
             }
         };
 
@@ -68,7 +98,7 @@ public class OfferRideFragment extends Fragment {
         timeIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showTimePicker();
+                showTimePicker(time.get("hour"),time.get("minute"));
             }
         });
 
@@ -120,23 +150,30 @@ public class OfferRideFragment extends Fragment {
     }
 
     // Mostra il DatePicker
-    private void showDatePicker() {
+    private void showDatePicker(Integer day,Integer month, Integer year) {
+        year = (year.equals(0) ? Calendar.getInstance().get(Calendar.YEAR) : year);
+        month = (month.equals(0) ? Calendar.getInstance().get(Calendar.MONTH) : month);
+        day = (day.equals(0) ? Calendar.getInstance().get(Calendar.DAY_OF_MONTH) : day);
+
         DatePickerDialog datePickerDialog = new DatePickerDialog(
                 requireContext(),
                 dateSetListener,
-                Calendar.getInstance().get(Calendar.YEAR),
-                Calendar.getInstance().get(Calendar.MONTH),
-                Calendar.getInstance().get(Calendar.DAY_OF_MONTH)
+                year,
+                month,
+                day
         );
         datePickerDialog.show();
     }
 
-    private void showTimePicker() {
+    private void showTimePicker(Integer hour, Integer minute) {
+        hour = (hour.equals(-1) ? Calendar.getInstance().get(Calendar.HOUR_OF_DAY) : hour);
+        minute = (minute.equals(-1) ? Calendar.getInstance().get(Calendar.MINUTE) : minute);
+
         TimePickerDialog timePickerDialog = new TimePickerDialog(
                 requireContext(),
                 timeSetListener,
-                Calendar.getInstance().get(Calendar.HOUR_OF_DAY),
-                Calendar.getInstance().get(Calendar.MINUTE),
+                hour,
+                minute,
                 true
         );
         timePickerDialog.show();
@@ -148,16 +185,23 @@ public class OfferRideFragment extends Fragment {
         TextInputEditText targaEditText = dialogView.findViewById(R.id.targa_edit_text);
         TextInputEditText dettagliEditText = dialogView.findViewById(R.id.dettagli_veicolo_edit_text);
 
+        targaEditText.setText(textTarga);
+        dettagliEditText.setText(textDettagli);
+
         new MaterialAlertDialogBuilder(getContext())
-                .setTitle("Title")
+                .setTitle("Dati veicolo")
                 .setView(dialogView)
                 .setPositiveButton("GOT IT", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        String targa = targaEditText.getText().toString();
-                        String dettagli = dettagliEditText.getText().toString();
+                        textTarga = targaEditText.getText().toString();
+                        textDettagli = dettagliEditText.getText().toString();
+                        if(!textTarga.equals("") && !textDettagli.equals("")) {
+                            fieldFilled[0] = true;
+                        }
+                        else fieldFilled[0] = false;
+                            setFieldColor();
 
-                        // Utilizza targa e dettagli come necessario
                     }
                 })
                 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
@@ -169,5 +213,12 @@ public class OfferRideFragment extends Fragment {
                 .show();
     }
 
+    private void setFieldColor() {
+        TextView textViewVeicolo = rootView.findViewById(R.id.textView_veicolo);
+        if(!fieldFilled[0])
+            textViewVeicolo.setTextColor(Color.RED);
+        else textViewVeicolo.setTextColor(Color.GREEN);
+    }
 
+    //fare anche le altre caselle ??
 }
