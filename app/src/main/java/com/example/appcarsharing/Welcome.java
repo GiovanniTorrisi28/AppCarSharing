7739a14108489291f9fe364b535c01fd15389cd3
GiovanniTorrisi28 <TRRGNN02A28C351N@studium.unict.it>
2023-08-26 16:27:30 +0200

    implementata pagina di benvenuto

---
+++ app/src/main/java/com/example/appcarsharing/Welcome.java
@@ -0,0 +1,36 @@
+package com.example.appcarsharing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button signInBtn = findViewById(R.id.sign_in_btn);
+        Button signUpBtn = findViewById(R.id.sign_up_btn);
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this, Login.class);
+                startActivity(intent);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Welcome.this,Registration.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
