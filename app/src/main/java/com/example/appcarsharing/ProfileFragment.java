b540093f7ed8e9a2ea2dac68022a55153c4e8e7c
GiovanniTorrisi28 <TRRGNN02A28C351N@studium.unict.it>
2023-09-08 18:11:50 +0200

    implementata autorizzazione in cambia foto

--- app/src/main/java/com/example/appcarsharing/ProfileFragment.java
+++ app/src/main/java/com/example/appcarsharing/ProfileFragment.java
@@ -2,18 +2,23 @@ package com.example.appcarsharing;
 
 import static android.app.Activity.RESULT_OK;
 
-import android.app.AlertDialog;
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.GradientDrawable;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -21,6 +26,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,6 +39,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -63,6 +70,7 @@ public class ProfileFragment extends Fragment {
     private FirebaseUser user;
     private ImageView imageView;
     private ProgressBar progressBar;
+    private static final int PERMISSION_REQUEST_READ_EXTERNAL_STORAGE = 1;
 
     public ProfileFragment() {
         auth = FirebaseAuth.getInstance();
@@ -111,14 +119,74 @@ public class ProfileFragment extends Fragment {
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*"); // Specifica che si desidera selezionare solo immagini
-                startActivityForResult(intent, 1);
-                 */
+                requestStoragePermission();
             }
         });
 
         return rootView;
+    }
+
+    private void openImagePicker() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, 1);
+    }
+
+    private void requestStoragePermission() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            //Permessi non concessi, richiedili
+            if (isPermissionPermanentlyDenied(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                // L'utente ha selezionato "Non chiedere mai più" per l'autorizzazione
+                showPermissionDeniedDialog();
+            }
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_READ_EXTERNAL_STORAGE);
+
+        } else {
+            // Hai già i permessi
+            openImagePicker();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // L'autorizzazione è stata concessa
+                openImagePicker();
+            } else {
+                // L'autorizzazione è stata negata
+            }
+        }
+    }
+
+    private boolean isPermissionPermanentlyDenied(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!shouldShowRequestPermissionRationale(permission)) {
+                return ActivityCompat.checkSelfPermission(getContext(), permission) != PackageManager.PERMISSION_GRANTED;
+            }
+        }
+        return false;
+    }
+
+    private void showPermissionDeniedDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+        builder.setTitle("Autorizzazione richiesta");
+        builder.setMessage("Per cambiare foto, devi concedere l'autorizzazione per accedere ai file. Vuoi aprire le impostazioni dell'app per abilitare l'autorizzazione?");
+        builder.setPositiveButton("Apri impostazioni", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                Uri uri = Uri.fromParts("package", getContext().getPackageName(), null);
+                intent.setData(uri);
+                startActivity(intent);
+            }
+        });
+        builder.setNegativeButton("Annulla", null);
+
+        AlertDialog dialog = builder.show();
+        dialog.show();
     }
 
     @Override
